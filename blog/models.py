7a4a2b0bb8750c66df18e2fbfa2ffc55ea2bf64a
Leonardo Barros <nickolback@gmail.com>
2016-01-03 14:28:05 -0200

    Adicionando o admin site no models

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib import admin
 
 class BlogPost(models.Model):
     title=models.CharField(max_length=150)
     body=models.TextField()
     time=models.DateTimeField()
+
+admin.site.register(BlogPost)
