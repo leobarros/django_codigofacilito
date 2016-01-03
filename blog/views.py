b961ab8045bff22e94dcfd48caa825c9f37e0399
Leonardo Barros <nickolback@gmail.com>
2016-01-03 14:58:05 -0200

    Criando views

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.template import loader, Context
+from django.http import HttpResponse
+from blog.models import BlogPost
+
+
+def archive(request):
+    posts = BlogPost.objects.all()
+    mi_template = loader.get_template("archive.html")
+    mi_contexto = Context({'posts':posts})
+    return HttpResponse(mi_template.render(mi_contexto))
